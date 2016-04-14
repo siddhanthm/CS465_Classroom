cdcd9aaa3655f276dc1971604e6496787b642492
Siddhanth Maheshwari <siddhanthm@outlook.com>
2016-04-14 04:01:13 -0500

    Login Pages

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,14 @@
-var app = angular.module('mp3',['ngRoute']);
+var app = angular.module('mp3',['ngRoute','classRoomControllers']);
 
 app.config(function ($routeProvider) {
-
-})
+	$routeProvider.when('/', {
+		templateUrl: './partials/FrontPage.html',
+		controller: 'FrontPageController'
+	}).when('/login', {
+		templateUrl: './partials/login.html',
+		controller: 'LoginCtrl'
+	}).when('/signup', {
+		templateUrl: './partials/signup.html',
+		controller: 'SignupCtrl'
+	}).otherwise({ redirectTo: '/' });
+});
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,17 @@
 /* Sample Controller */
+var classRoomControllers = angular.module('classRoomControllers', []);
+
+classRoomControllers.controller('FrontPageController', ['$scope','$http', function($scope, $http){
+
+	console.log("Here");
+}]);
+
+classRoomControllers.controller('LoginCtrl', ['$scope','$http', function($scope, $http){
+
+	console.log("Here");
+}]);
+
+classRoomControllers.controller('SignupCtrl', ['$scope','$http', function($scope, $http){
+
+	console.log("Here");
+}]);
